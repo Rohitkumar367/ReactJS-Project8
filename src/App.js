9eed2b271cddc21869f6763cd5e0b24a6311619d
Rohitkumar367 <rohitddroid@gmail.com>
2023-12-30 22:33:37 +0530

    Blogs component done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Blogs from './components/Blogs';
 import Pagination from './components/Pagination';
+import { AppContext } from './context/AppContext';
 
 function App() {
+
+  const {fetchBlogPosts} = useContext(AppContext);
+
+  useEffect(() => {
+    fetchBlogPosts();
+  },[])
+
   return (
     <div>
       <Header></Header>
--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import Spinner from './Spinner';
 
 const Blogs = () => {
+
+  // Step3 => consuming
+  const {loading, posts} = useContext(AppContext);
+
+
   return (
     <div>
+
+      {
+        loading ? (<Spinner></Spinner>) : (posts.length===0 ? 
+            (
+              <div>
+                <p>No post found</p>
+              </div>
+            )
+             : 
+            (
+              posts.map((curElem) => {
+                return(
+                  <div key={curElem.id}>
+
+                    <p className='font-bold'>{curElem.title}</p>
+
+                    <p>
+                      By <span>{curElem.author}</span> on <span>{curElem.category}</span>
+                    </p>
+
+                    <p>Posted on {curElem.date}</p>
+
+                    <p>{curElem.content}</p>
+
+                    <div>
+                      {curElem.tags.map((tag, index) => {
+                        return (
+                          <span key={index}>{`#${tag}`}</span>
+                        )
+                      })}
+                    </div>
+
+                  </div>
+                )
+              })
+            )
+        )
+      }
       
     </div>
   )
